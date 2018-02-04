a9883e8015fc14183524e43dfcd1a6bacccbe293
Louis Heim <heimlouis@gmail.com>
2018-02-03 18:33:34 -0600

    added html textboxes and ids

---
+++ Week1Weekend.js
@@ -0,0 +1,30 @@
+// ====================================================================
+// # Weekend Challenge 1 - Salary Calculator:
+// Create an application that records employee salaries and adds
+// salaries up to report monthly costs.
+// ====================================================================
+//
+// ## Topics Covered:
+// - JavaScript
+// - jQuery - Selectors, append, and event handling
+// ====================================================================
+//
+// ## Assignment:
+// The application should have an input form that collects
+//  - employee first name
+//  - last name
+//  - ID number
+//  - job title
+//  - annual salary
+//
+// A 'Submit' button should collect the form information, store the
+// information to calculate monthly costs, append information to the
+// DOM and clear the input fields. Using the stored information,
+// calculate monthly costs and append this to the to DOM.
+// ====================================================================
+
+//needed:
+//input textboxes (first name, last name, ID number, job title & annual salary)
+//submit button (this will need an ID for on click)
+//on click will need to collect data, store info, append info on the DOM,
+//clear input textboxes, calculate monthly cost & append monthly cost to DOM
