14332cc47acc359f2c6b5b1a047e2ff6b53088fa
Louis Heim <heimlouis@gmail.com>
2018-02-03 19:43:34 -0600

    added displayEmployees and employeeDataObject within the document ready function

--- Week1Weekend.js
+++ Week1Weekend.js
@@ -29,13 +29,50 @@
 //submit button (this will need an ID for on click)
 //on click will need to collect data, store info, append info on the DOM,
 //clear input textboxes, calculate monthly cost & append monthly cost to DOM
+console.log('js');
 //employee data - object
-var employeeData = {
-  firstName: '',
-  lastName: '',
-  idNumber: 0,
-  jobTitle: '',
-  annualSalary: 0
-};
-//employee - array
+
+//employees - array
 var employees = [];
+
+$(document).ready( function(){
+  console.log('jq is ready');
+  $('#expenseCalcBtn').on( 'click', function(){
+    console.log('in expenseCalcBtn on click');
+    // get user input
+    //create new employeeData object
+    var employeeDataObject = {
+      firstName: $('#firstName').val(),
+      lastName: $('#lastName').val(),
+      idNumber: $('#idNumber').val(),
+      jobTitle: $('#jobTitle').val(),
+      annualSalary: $('#annualSalary').val()
+    };//end employeeData object
+    console.log('adding:',employeeDataObject);
+    //push to object array
+    employees.push(employeeDataObject);
+    //display on the DOM
+    displayEmployees();
+  });
+});
+//start displayEmployees function
+function displayEmployees(){
+  console.log('in displayEmployees');
+  //add to the DOM in the ul element in the array
+  var employeeOutput = $('#employeeDataID');
+  //need to empty the ul
+  employeeOutput.empty();
+  //loop through the employees array
+  for (var i = 0; i < employees.length; i++) {
+    //these need to be appended to the ul element
+    //local variable to store the append data
+    var employeeStringData = '<li>';
+    employeeStringData += employees[i].firstName+' ';
+    employeeStringData += employees[i].lastName+' ';
+    employeeStringData += employees[i].idNumber+' ';
+    employeeStringData += employees[i].jobTitle+' ';
+    employeeStringData += employees[i].annualSalary+' ';
+    employeeStringData += '</li>';
+    employeeOutput.append(employeeStringData);
+  }
+}
